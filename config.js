ccf1bcd38f00dd5a6b783f5b3698b1e0baa82f37
Chienmo <chienbnmt@gmail.com>
2024-02-20 21:55:17 +0700

    code web cho miu

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Thuỷ xinh gái à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu anh ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là yêu Chiển <33',
+    btnNo: 'Không, Anh yêu à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Không yêu Chiển thì yêu ai. Mãi yêu Chiển <3',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé Thuỷ yêu dấu của anh',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/chienchills/' 
+}
